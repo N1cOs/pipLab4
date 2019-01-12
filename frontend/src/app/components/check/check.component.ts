7e80eda76364719f15f5472753b21f74273eceb2
Nick <nickkarmatskikh@gmail.com>
2019-01-13 02:02:04 +0300

    added history dots

--- frontend/src/app/components/check/check.component.ts
+++ frontend/src/app/components/check/check.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit,} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild,} from '@angular/core';
 import {CheckService} from '../../services/check.service';
 import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 import {Check} from '../../interfaces/check';
@@ -9,7 +9,11 @@ import {Check} from '../../interfaces/check';
   templateUrl: './check.component.html',
   styleUrls: ['./check.component.css']
 })
-export class CheckComponent implements OnInit {
+export class CheckComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('myCanvas')
+  canvasRef:ElementRef;
+
 
   readonly xMin:number = -3;
   readonly xMax:number = 5;
@@ -23,7 +27,7 @@ export class CheckComponent implements OnInit {
 
   coordinatesForm:FormGroup;
 
-  history:Check[];
+  history:Check[] = [];
 
 
   constructor(private fb:FormBuilder, private checkService: CheckService) {
@@ -42,13 +46,16 @@ export class CheckComponent implements OnInit {
   }
 
   ngOnInit() {
-    for(let i = this.xMin; i <= this.xMax; i++)
+    for (let i = this.xMin; i <= this.xMax; i++)
       this.xValues.push(i);
 
-    for(let i = this.rMin; i <= this.rMax; i++)
+    for (let i = this.rMin; i <= this.rMax; i++)
       this.rValues.push(i);
+  }
 
+  ngAfterViewInit(): void {
     this.initHistory();
+    this.drawCanvas();
   }
 
   onSubmit(check:any){
@@ -60,6 +67,7 @@ export class CheckComponent implements OnInit {
     this.checkService.check(request, localStorage.getItem('token'))
       .subscribe((data:Check) => {
       this.history.splice(0, 0, data);
+      this.historyDots();
     });
   }
 
@@ -67,7 +75,74 @@ export class CheckComponent implements OnInit {
     this.checkService.checkHistory(localStorage.getItem('token'))
       .subscribe((results:Check[]) =>{
         this.history = results;
+        this.historyDots();
       });
   }
 
+  drawCanvas(){
+    const canvas = this.canvasRef.nativeElement;
+    const width = canvas.width;
+    const height = canvas.height;
+    const coordCenter = width / 2;
+    const scale = 20;
+    const radius = 100;
+    const ctx = canvas.getContext('2d');
+
+    ctx.beginPath();
+    let startangel = 90 * Math.PI / 180;
+    let endangel = 180 * Math.PI / 180;
+    ctx.arc(coordCenter, coordCenter, radius, startangel, endangel, false);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+    ctx.moveTo(width / 2, height / 2 + radius);
+    ctx.lineTo(width / 2 - radius, height / 2);
+    ctx.lineTo(width / 2, height / 2);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.rect(width / 2, height / 2 - radius, radius / 2, radius);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(width / 2, height / 2 - radius / 2);
+    ctx.lineTo(width / 2 - radius, height / 2);
+    ctx.lineTo(width / 2, height / 2);
+    ctx.fill();
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, height / 2);
+    ctx.lineTo(width, height / 2);
+    ctx.strokeStyle = '#000';
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(width / 2, 0);
+    ctx.lineTo(width / 2, height);
+    ctx.stroke();
+
+    ctx.fillStyle = '#ed1c24';
+  }
+
+  historyDots(){
+    const canvas = this.canvasRef.nativeElement;
+    const width = canvas.width;
+    const height = canvas.height;
+    const scale = 20;
+    const ctx = canvas.getContext('2d');
+
+    for(let check of this.history){
+      ctx.beginPath();
+      ctx.arc(check.xValue * scale + width / 2,
+        height / 2 - check.yValue * scale,
+        2,
+        0,
+        Math.PI * 2);
+      if(check.result)
+        ctx.fillStyle = '#1f4';
+      else
+        ctx.fillStyle = '#ed1c24';
+      ctx.fill();
+    }
+  }
 }
